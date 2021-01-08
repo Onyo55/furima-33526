cf95779fbb7be9ab36a123b7ff51efed8c2a090e
Onyo55 <dayeyada@gmail.com>
2021-01-08 12:38:06 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  with_options presence: true do
+    validates :nickname
+    validates :email,                 uniqueness: true
+    validates :encrypted_password,    format: {with: /\A(?=.*?[a-z])(?=.*?[\b])[a-z\b]/i}
+    validates :first_name,            format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/}
+    validates :last_name,             format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/}
+    validates :first_furigana,        format: {with: /\A[ァ-ヶ]+\z/}
+    validates :last_furigana,         format: {with: /\A[ァ-ヶ]+\z/}
+    validates :birthday
+ end
+
+  has_many :items
+  has_many :purchases
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
