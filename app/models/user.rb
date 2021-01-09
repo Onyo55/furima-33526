93617c7ab5e84357c3ea373fbe1ffdef80d9ef49
Onyo55 <dayeyada@gmail.com>
2021-01-09 19:36:40 +0900

    rubocop -a 実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,13 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname
     validates :email,                 uniqueness: { case_sensitive: false }
-    validates :password,              format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
-    validates :first_name,            format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/}
-    validates :last_name,             format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/}
-    validates :first_furigana,        format: {with: /\A[ァ-ヶ]+\z/}
-    validates :last_furigana,         format: {with: /\A[ァ-ヶ]+\z/}
+    validates :password,              format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+    validates :first_name,            format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
+    validates :last_name,             format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
+    validates :first_furigana,        format: { with: /\A[ァ-ヶ]+\z/ }
+    validates :last_furigana,         format: { with: /\A[ァ-ヶ]+\z/ }
     validates :birthday
- end
+  end
 
   has_many :items
   has_many :purchases
