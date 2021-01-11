3ea7ef5a7a98279a3829294d466e2757a4bc7a24
Onyo55 <dayeyada@gmail.com>
2021-01-11 17:02:10 +0900

    itemモデルのバリデーション、アソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,23 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :name
+    validates :price, numericality: {greater_than_or_equal_to:300, less_than_or_equal_to:9999999}
+    validates :text
+    with_options numericality: {other_than:1} do
+      validates :category_id
+      validates :state_id
+      validates :prefecture_id
+      validates :ship_day_id
+      validates :ship_fee_id
+    end
+  end
+
+  belongs_to :user
+  has_one :purchase
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :Prefecture
+  belongs_to :ship_day
+  belongs_to :ship_fee
+  belongs_to :state
 end
