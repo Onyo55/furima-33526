6c4f83a8c81d80a66db6a53d5ba657eb626f312c
Onyo55 <dayeyada@gmail.com>
2021-01-15 19:24:46 +0900

    charge_formモデルのテストコード作成

--- app/models/charge_form.rb
+++ app/models/charge_form.rb
@@ -2,15 +2,15 @@ class ChargeForm
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :block, :building, :phone_number, :user_id, :item_id, :token
 
-  with_options presence: true do
-    validates :postal_code
+  with_options presence: true do 
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/}
     validates :prefecture_id
     validates :city
     validates :block
-    validates :phone_number
+    validates :phone_number, format: { with: /\A\d{1,11}\z/ }
     validates :token
   end
-
+  
   def save
     order = Order.create(user_id: user_id, item_id: item_id )
     Address.create(
